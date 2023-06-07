3cc2b55239261a2fcc4a79668d7bddc99d872be1
tandy <tandy1229@qq.com>
2023-06-07 23:06:12 +0800

    feat: update autopairs work with cmp

--- lua/plugin/autopairs.lua
+++ lua/plugin/autopairs.lua
@@ -1,3 +1,7 @@
-local remap = vim.api.nvim_set_keymap
-local npairs = require('nvim-autopairs')
-npairs.setup({ map_cr = false })
+require("nvim-autopairs").setup {}
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
