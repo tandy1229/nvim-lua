129bbe5cc3fa234d8e72d59df5fe785e5d801016
tandy <tandy1229@qq.com>
2023-07-17 17:24:42 +0800

    feat: update the main init.lua

--- init.lua
+++ init.lua
@@ -1,21 +1,8 @@
 --
--- init files for neovim 0.8.0+
---
+-- init files for neovim 0.9.0+
+--     
 --
 
 vim.loader.enable()
 require('core.defaults')
-
--- plugin manage lazy
 require('init.lazy')
-
--- vim files load
--- vim.api.nvim_command('runtime init/init.vim')
-
--- personal setting
--- g.python3_host_prog = '/usr/local/bin/python3.11'
-
---     
---
--- use the global statusline
--- vim.o.laststatus = 3
