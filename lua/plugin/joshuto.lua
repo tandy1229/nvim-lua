837436b0ae459f6b490645d413b9542286c67094
tandy <tandy1229@qq.com>
2023-07-28 22:55:02 +0800

    feat: update joshuto.nvim setting

--- lua/plugin/joshuto.lua
+++ lua/plugin/joshuto.lua
@@ -1,1 +1,4 @@
+vim.g.joshuto_floating_window_scaling_factor = 1.0
+vim.g.joshuto_use_neovim_remote = 1
+vim.g.joshuto_floating_window_winblend = 0
 vim.keymap.set('n', 'R', ':Joshuto<CR>', { silent = true })
