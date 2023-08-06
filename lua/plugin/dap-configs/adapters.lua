46764018b1f482ee0f97417e068ebabb133c23a2
tandy <tandy1229@qq.com>
2023-08-06 14:17:09 +0800

    feat: add vscode-lldb debuger

--- lua/plugin/dap-configs/adapters.lua
+++ lua/plugin/dap-configs/adapters.lua
@@ -8,4 +8,10 @@ adapters.nlua = function(callback, config)
 	})
 end
 
+adapters.lldb = {
+	type = 'executable',
+	command = '/usr/local/opt/llvm/bin/lldb-vscode', -- adjust as needed, must be absolute path
+	name = 'lldb',
+}
+
 return adapters
