beab08f35517e2dda63be07fede1a53dcdb28d37
tandy <tandy1229@qq.com>
2023-08-20 21:13:41 +0800

    feat: remove the sheckcheck diagnostics

--- lua/plugin/lsp/null-ls.lua
+++ lua/plugin/lsp/null-ls.lua
@@ -13,7 +13,7 @@ null_ls.setup({
 		null_ls.builtins.code_actions.gitsigns,
 		null_ls.builtins.code_actions.gitrebase,
 		null_ls.builtins.code_actions.shellcheck,
-		null_ls.builtins.diagnostics.shellcheck,
+		-- null_ls.builtins.diagnostics.shellcheck,
 		null_ls.builtins.formatting.shfmt,
 		null_ls.builtins.diagnostics.markdownlint,
 		null_ls.builtins.formatting.markdown_toc,
