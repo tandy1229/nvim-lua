4d72100750c3dc0438877d79993e14182ca47a66
tandy <tandy1229@qq.com>
2023-09-04 10:33:57 +0800

    feat(treesitter): make the autotag works

--- lua/plugin/autotag.lua
+++ lua/plugin/autotag.lua
@@ -1,5 +1,9 @@
+require('nvim-ts-autotag').setup()
 require('nvim-treesitter.configs').setup({
 	autotag = {
 		enable = true,
+		enable_rename = true,
+		enable_close = true,
+		enable_close_on_slash = true,
 	},
 })
