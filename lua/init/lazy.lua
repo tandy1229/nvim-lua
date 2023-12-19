d53d253c2a02760a09cad2d928fcbd2e048db2e9
tandy <tandy1229@qq.com>
2023-12-19 09:27:15 +0800

    feat: add conform.nvim

--- lua/init/lazy.lua
+++ lua/init/lazy.lua
@@ -557,6 +557,40 @@ require('lazy').setup({
 	},
 
 	{
+		'stevearc/conform.nvim',
+		event = { 'BufWritePre' },
+		cmd = { 'ConformInfo' },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				'<leader>f',
+				function()
+					require('conform').format({ async = true, lsp_fallback = true })
+				end,
+				mode = '',
+				desc = 'Format buffer',
+			},
+		},
+		-- Everything in opts will be passed to setup()
+		opts = {
+			-- Define your formatters
+			formatters_by_ft = {
+				lua = { 'stylua' },
+				python = { 'isort', 'black' },
+				javascript = { { 'prettierd', 'prettier' } },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { '-i', '2' },
+				},
+			},
+		},
+	},
+
+	{
 		-- showing the lsp diagnostics
 		'folke/trouble.nvim',
 		event = { 'BufReadPre', 'FileType' },
